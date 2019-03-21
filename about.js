dbef2a2d9976f9126398414c769f6d690f1f58af
haiming.zeng <haiming.zeng@msxf.com>
2019-03-21 12:12:26 +0800

    修改about 和news

--- about.js
+++ about.js
@@ -1,3 +1,13 @@
 var a = '123';
 var f = '123';
 var d = '123';
+
+
+function logger(message, type = 'trace') {
+  axios.post('/logger', {message: message, page: location.href, type})
+  .then(function (response) {
+  })
+  .catch(function (error) {
+	console.log(error)
+  })
+}
